52578ffa392110af40b044cf9ac5aa369a31db0c
SAPEELYSAIVIVEK <22h51a05j2@cmrcet.ac.in>
2024-11-22 20:25:27 +0530

    commited 2nd time

--- file.java
+++ file.java
@@ -1,6 +1,6 @@
 class file{
   public static void main(String[] args){
 
-    System.out.println("this is the devops projects that am working on");
+    System.out.println("this is the devops projects that am working on 2ND TIME");
   }
 }
